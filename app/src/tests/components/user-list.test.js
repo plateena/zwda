2d2696dc14a29dd70631abd64187873fa342d8a7
zainundin <zainundin.mohammad@gmail.com>
2024-05-03 16:09:39 +0800

    test: fix user-list test

--- app/src/tests/components/user-list.test.js
+++ app/src/tests/components/user-list.test.js
@@ -1,15 +1,20 @@
-import UserList, { getServerSideProps } from '@/components/admin/users-list'
-import { render } from '@testing-library/react'
+import { getServerSideProps } from '@/pages/admin/users'
 
 const unmockedFetch = global.fetch
 
 describe('User list components', () => {
     it('should call the api on load', async () => {
-        const apiUrl = 'https://reqres.in/api/users'
-        const mockData = ['hello']
+        const apiUrl = 'https://reqres.in/api/users?page=1'
+        const users = [
+            { id: 1, first_name: 'George', last_name: 'Washington' },
+            { id: 2, first_name: 'John', last_name: 'Adams' },
+            { id: 3, first_name: 'Thomas', last_name: 'Jefferson' },
+            { id: 4, first_name: 'James', last_name: 'Madison' },
+            { id: 5, first_name: 'George', last_name: 'Bush' },
+        ]
         global.fetch = jest.fn(() =>
             Promise.resolve({
-                json: () => Promise.resolve({ test: 100 }),
+                json: () => Promise.resolve({ data: users }),
             })
         )
 
