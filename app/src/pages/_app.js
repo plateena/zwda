cfcf9b78af0be4f7b851c4cef204e765ec257b7c
zainundin <zainundin.mohammad@gmail.com>
2024-05-03 15:43:49 +0800

    feat: update landing page ui

--- app/src/pages/_app.js
+++ app/src/pages/_app.js
@@ -12,7 +12,7 @@ export default function App({ Component, pageProps }) {
                 <ReduxProvider>
                     <div className="flex flex-col min-h-screen">
                         <Header />
-                        <main className="flex-grow flex items-center justify-center">
+                        <main className="flex-grow flex items-center justify-center mx-12 py-8 mb-8">
                             <Component {...pageProps} />
                         </main>
                         <Footer />
--- app/src/pages/index.js
+++ app/src/pages/index.js
@@ -3,13 +3,53 @@ import useAuth from '@/utils/auth'
 
 export default function Home() {
     const auth = useAuth()
+
     return (
-        <>
+        <div className="flex flex-col items-center justify-center h-screen">
             {auth.isLoggedIn() ? (
-                <span> Welcome, {auth.getUser()?.name}</span>
+                <div className="flex flex-col items-center">
+                    <img
+                        src={auth.getUser()?.image}
+                        alt={auth.getUser()?.name}
+                        className="w-32 h-32 rounded-full mb-4"
+                    />
+                    <span className="text-lg font-semibold mb-2">
+                        Welcome, {auth.getUser()?.name}
+                    </span>
+                </div>
             ) : (
-                <Link href="/login">Login</Link>
+                <div>
+                    <h1 className="text-4xl font-semibold mb-4">
+                        Welcome to Our Website
+                    </h1>
+                    <p className="text-lg mb-8">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Sed do eiusmod tempor incididunt ut labore et dolore
+                        magna aliqua. Ut enim ad minim veniam.
+                    </p>
+                    <p className="text-lg mb-8">
+                        Lorem ipsum dolor sit amet, officia excepteur ex fugiat
+                        reprehenderit enim labore culpa sint ad nisi Lorem
+                        pariatur mollit ex esse exercitation amet. Nisi anim
+                        cupidatat excepteur officia. Reprehenderit nostrud
+                        nostrud ipsum Lorem est aliquip amet voluptate voluptate
+                        dolor minim nulla est proident. Nostrud officia pariatur
+                        ut officia. Sit irure elit esse ea nulla sunt ex
+                        occaecat reprehenderit commodo officia dolor Lorem duis
+                        laboris cupidatat officia voluptate. Culpa proident
+                        adipisicing id nulla nisi laboris ex in Lorem sunt duis
+                        officia eiusmod. Aliqua reprehenderit commodo ex non
+                        excepteur duis sunt velit enim. Voluptate laboris sint
+                        cupidatat ullamco ut ea consectetur et est culpa et
+                        culpa duis.
+                    </p>
+                    <Link href="/login">
+                        <span className="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 transition-colors duration-300">
+                            Login
+                        </span>
+                    </Link>
+                </div>
             )}
-        </>
+        </div>
     )
 }
